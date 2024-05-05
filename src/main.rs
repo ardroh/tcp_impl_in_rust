dc7cc95634a315ce50df995578c831b03278d53b
ardroh <ardroh@gmail.com>
2024-05-05 20:45:37 +0200

    Capture only IPv4.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,23 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let nic = tun_tap::Iface::without_packet_info("tun0", tun_tap::Mode::Tun)?;
+    let nic = tun_tap::Iface::new("tun0", tun_tap::Mode::Tun)?;
     let mut buf = vec![0u8; 1504];
-    let nbytes = nic.recv(&mut buf[..])?;
-    eprintln!("read {} bytes: {:x?}", nbytes, &buf[..nbytes]);
+    loop {
+        let nbytes = nic.recv(&mut buf[..])?;
+        let eth_flags = u16::from_be_bytes([buf[0], buf[1]]);
+        let eth_proto = u16::from_be_bytes([buf[2], buf[3]]);
+        //https://en.wikipedia.org/wiki/EtherType
+        const ETH_P_IP: u16 = 0x0800;
+        if eth_proto != ETH_P_IP {
+            // not IPv4
+            continue;
+        }
+        eprintln!(
+            "read {} bytes, flags: {:x}, proto: {:x} bytes: {:x?}",
+            nbytes - 4,
+            eth_flags,
+            eth_proto,
+            &buf[4..nbytes]
+        );
+    }
     Ok(())
 }
