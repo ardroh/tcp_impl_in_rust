6c2f1fce0d4b5c5c16e58a9807aa17fc293dc740
ardroh <ardroh@gmail.com>
2024-05-05 20:45:23 +0200

    Add signal trap.

--- run.sh
+++ run.sh
@@ -11,5 +11,5 @@ sudo setcap cap_net_admin=eip ./target/release/tcp_impl_in_rust
 pid=$!
 sudo ip addr add 192.168.0.1/24 dev tun0
 sudo ip link set up dev tun0
+trap "kill $pid" INT TERM
 wait $pid
-
